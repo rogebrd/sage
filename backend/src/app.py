17b49f421522358889a205fea8bee17519ab0231
Brad Rogers <brad12rogers@gmail.com>
2021-12-08 16:38:05 -0500

    [Backend] Refactor source code to live under src/ - Move app.py and sage.py to live under src/ - Add requests to requirements.txt

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,14 +1,45 @@
 from flask import Flask, request
+from stock_client import StockClient
+from token_client import TokenClient
 from flask_cors import CORS
 
 from sage import create_account, create_transaction, get_home
+
+from logging.config import dictConfig
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+
 app = Flask(__name__)
 CORS(app)
+
+# Create Clients
+token_client = TokenClient()
+stock_client = StockClient(token_client)
 
 logger = app.logger
 
 @app.route('/')
 def handle_base():
+    logger.info("testing stock client")
+    stock_client.fetch_prices(["AAPL", "GOOG"])
+    logger.info(stock_client.current_prices)
+    logger.info(stock_client.update_time)
     return "Welcome to Sage"
 
 @app.route('/api/', methods = ['GET'])
@@ -30,4 +61,4 @@ def handle_transaction():
 if __name__ == '__main__':
     app.logger.info("Spinning up main server")
     app.run(debug=True, host='0.0.0.0')
-    #app.run()
+    
