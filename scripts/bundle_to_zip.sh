8748fca8c3cc5b5ea5abdf38db81e9e504663eca
Brad Rogers <brad12rogers@gmail.com>
2021-12-08 16:39:22 -0500

    [Backend] Update bundle script to reflect new src tree

--- scripts/bundle_to_zip.sh
+++ scripts/bundle_to_zip.sh
@@ -1,6 +1,7 @@
 cd ../backend
 rm sage.zip
 zip -r sage.zip .ebextentions 
-zip -g sage.zip sage.py app.py Dockerfile requirements.txt docker-compose.yml
+zip -r sage.zip src
+zip -g sage.zip Dockerfile requirements.txt docker-compose.yml
 aws s3 cp sage.zip s3://sage-codedeployment-bucket/build/eb/
 cd ../scripts
